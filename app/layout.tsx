b4f6ec4578b0b571545d65efa3422457bed639e9
chatinunk97 <chatinun.hobby@gmail.com>
2024-07-17 17:23:13 +0900

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className}`}>{children}
+
+      <h1>This is the root layout!</h1>
+      </body>
+      
     </html>
   );
 }
